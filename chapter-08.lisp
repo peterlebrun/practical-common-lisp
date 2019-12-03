1de294de094e99bc6ddccd4ee50c6edfbda44edc
peterlebrun <peterlebrun@gmail.com>
2019-12-03 15:42:18 -0500

    Chapter 8: doprimes macro

---
+++ chapter-08.lisp
@@ -0,0 +1,15 @@
+(defun primep (number)
+  (when (> number 1)
+    (loop for fac from 2 to (isqrt number) never
+         (zerop (mod number fac)))))
+
+(defun next-prime (number)
+  (loop for n from number when (primep n) return
+     n))
+
+(defmacro do-primes ((var start end) &body body)
+  (let ((ending-value-name (gensym)))
+    `(do ((,var (next-prime ,start) (next-prime (1+ ,var)))
+          (,ending-value-name ,end))
+         ((> ,var ,ending-value-name))
+       ,@body)))
