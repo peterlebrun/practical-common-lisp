b523847eeefa7fce534e6622fc1891ffbc41955b
peterlebrun <peterlebrun@gmail.com>
2019-12-05 17:38:21 -0500

    Not currently working but I gotta run

---
+++ chapter-09.lisp
@@ -0,0 +1,22 @@
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a%" result form)
+  result)
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+      ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+      ,result)))
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defun test-+ ()
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -4)))
+
+(defun reload-ch-09 ()
+  (load "~/eng/github.com/peterlebrun/practical-common-lisp/chapter-09.lisp"))
